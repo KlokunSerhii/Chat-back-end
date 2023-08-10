f3e8efe45b8a02c78de7816cae02ed178de56cd3
Serhii Klokun <99805859+KlokunSerhii@users.noreply.github.com>
2023-08-10 07:48:06 +0000

    refact

--- server.js
+++ server.js
@@ -1,15 +1,32 @@
 const mongoose = require('mongoose');
-const server = require('./app');
+const express = require('express')
+const app = express();
+const server = require('http').Server(app);
+const useSocket = require('socket.io');
+const io = useSocket(server)
+
+require('dotenv').config();
 
 const { DB_HOST, PORT } = process.env;
 
-mongoose
-  .connect(DB_HOST)
-  .then(() => {
-    server.listen(PORT);
-    console.log('Database connection successful');
-  })
-  .catch(error => {
-    console.log(error.message);
-    process.exit(1);
+app.get('/chat',(req, res)=>{
+res.json("HI")
+})
+
+io.on('connection', socket => {
+  socket.on('chat-message', message => {
+    socket.broadcast.emit('chat-message', message);
   });
+});
+
+mongoose
+.connect(DB_HOST)
+.then(() => {
+  server.listen(PORT);
+  console.log('Database connection successful');
+})
+.catch(error => {
+  console.log(error.message);
+  process.exit(1);
+});
+
