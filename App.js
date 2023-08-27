610d7c4584078f24be0170ac5a4c5c4934b052b8
Serhii Klokun <klokuns@gmail.com>
2023-08-27 23:04:39 +0300

    cors

--- App.js
+++ App.js
@@ -26,6 +26,22 @@ const formatsLogger =
 app.use(logger(formatsLogger));
 app.use(express.json());
 app.use(cors('*'));
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With'
+  );
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type'
+  );
+  res.header(
+    'Access-Control-Allow-Methods',
+    'PUT, GET, POST, DELETE, OPTIONS'
+  );
+  next();
+});
 
 app.use('/chat/users', authRouter);
 
