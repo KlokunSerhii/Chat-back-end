b1da7af89ac78e6af3f374fcbcfe29f37a5ce989
Serhii Klokun <klokuns@gmail.com>
2023-08-27 23:40:15 +0300

    fix

--- App.js
+++ App.js
@@ -25,19 +25,6 @@ const formatsLogger =
 
 app.use(logger(formatsLogger));
 app.use(express.json());
-router.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Max-Age', '1800');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'content-type'
-  );
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'PUT, POST, GET, DELETE, PATCH, OPTIONS'
-  );
-});
 app.use(cors());
 app.use('/chat/users', authRouter);
 
