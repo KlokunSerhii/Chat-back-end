166870f582e4b88e32de97b08dd179058265d6d9
Serhii Klokun <klokuns@gmail.com>
2023-09-04 20:36:54 +0300

    up

--- routes/api/message.js
+++ routes/api/message.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const { authenticate } = require('../../middelwares');
 const ctrl = require('../../controllers/message');
 
-router.get('/chat/:id', authenticate, ctrl.getAllMessage);
-router.post('/chat/:id', authenticate, ctrl.addMessage);
+// router.get('/chat/:id', authenticate, ctrl.getAllMessage);
+// router.post('/chat/:id', authenticate, ctrl.addMessage);
+
+router.get('/:id', authenticate, ctrl.getAllMessage);
+router.post('/:id', authenticate, ctrl.addMessage);
 
 module.exports = router;
