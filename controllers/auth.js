4d4a501e23492d22e3da696f02b9d7f22dd502c0
Serhii Klokun <klokuns@gmail.com>
2023-09-06 21:11:38 +0300

    avatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,14 @@
 const gravatar = require('gravatar');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+const jimp = require('jimp');
+const path = require('path');
+const fs = require('fs/promises');
 
 const { HttpError, ctrlWrapper } = require('../helpers');
 const { User } = require('../models/user');
 const { SECRET_KEY } = process.env;
+const avatarsDir = path.join(__dirname, '../', 'public', 'avatars');
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -41,18 +45,12 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne(
-    { email },
-    '-createdAt -updatedAt'
-  );
+  const user = await User.findOne({ email }, '-createdAt -updatedAt');
   if (!user) {
     throw HttpError(401, 'Email or password invalid');
   }
 
-  const passwordCompare = await bcrypt.compare(
-    password,
-    user.password
-  );
+  const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
     throw HttpError(401, 'Email or password invalid');
   }
@@ -96,9 +94,36 @@ const getCurrent = async (req, res) => {
   });
 };
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+
+  if (!_id) {
+    throw HttpError(401);
+  }
+  const { path: tempDir, originalname } = req.file;
+  jimp
+    .read(tempDir)
+    .then(avatar => {
+      return avatar.cover(250, 250).write(resultUpload);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  const userAvatar = `${_id}_${originalname}`;
+  const resultUpload = path.join(avatarsDir, userAvatar);
+  await fs.rename(tempDir, resultUpload);
+  const avatarURL = path.join('avatars', userAvatar);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({
+    avatarURL,
+  });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
   logout: ctrlWrapper(logout),
   getCurrent: ctrlWrapper(getCurrent),
+  updateAvatar: ctrlWrapper(updateAvatar),
 };
